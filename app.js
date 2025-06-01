829df54565f48d4724a6129018f549e74d8669be
oseijoel6111 <oseijoel6111@gmail.com>
2025-06-01 11:58:44 -0500

    feat: update user signup response to return request body

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ import {userRouter, noteRouter} from './routes/index.js'
 
 // create express app
 const app = express()
+// allows JSON
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
 const API_VERSION = '/api/v1'
 
 app.use(`${API_VERSION}/users`, userRouter)
-app.use(`${API_VERSION}/notes`, noteRouter )
-
-
+app.use(`${API_VERSION}/notes`, noteRouter)
 
 export default app;
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,8 @@
 
 
 export const signup = (req, res)=>{
-     res.send('User signup successfully')
+
+     res.send(req.body)
 }
 
 
