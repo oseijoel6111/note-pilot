d032ca189d71802fda9f5b16a2c1d2c31cae4861
oseijoel6111 <oseijoel6111@gmail.com>
2025-06-14 05:19:43 -0500

    feat: set up user and note routes in the Express app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import {userRouter, noteRouter} from './routes/index.js'
 
 // create express app
 const app = express()
@@ -8,5 +9,7 @@ app.use(express.urlencoded({extended: false}))
 
 const API_VERSION = '/api/v1'
 
+app.use(`${API_VERSION}/users`, userRouter)
+app.use(`${API_VERSION}/notes`, noteRouter)
 
 export default app;
